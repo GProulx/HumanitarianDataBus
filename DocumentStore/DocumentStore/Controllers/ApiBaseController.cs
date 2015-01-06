e535e4ccffa9f4c3a25ee3a8b9a0e42bb5a2fc38
drmyersii <drmyersii@users.noreply.github.com>
2015-01-06 15:35:50 -0500

    Initial commit of json_spike Web API

---
+++ DocumentStore/DocumentStore/Controllers/ApiBaseController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DocumentStore.Controllers
+{
+    // This allows us to easily extend ApiBaseController for ApiController access instead of extending 
+    // ApiController directly to ensure authorization on all api requests.
+    [Authorize]
+    public class ApiBaseController : ApiController
+    {
+
+    }
+}
---
+++ DocumentStore/DocumentStore/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocumentStore.Controllers
+{
+    // This allows us to easily extend BaseController for Controller access instead of extending 
+    // Controller directly to ensure authorization on all standard requests.
+    [Authorize]
+    public class BaseController : Controller
+    {
+
+    }
+}
